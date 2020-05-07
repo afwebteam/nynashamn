007cc099251d34fc55bd5e1dc89fd79c1a05a635
isse <anders.israelsson@afconsult.com>
2020-05-07 15:26:27 +0200

    Init

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+/* global exports */
+
+(() => {
+
+    'use strict';
+
+    //----- GULP -----
+    const
+        {
+            series,
+            parallel,
+            src,
+            dest,
+            watch
+        } = require('gulp'),
+
+        autoprefixer = require('gulp-autoprefixer'),
+        connect = require('gulp-connect'),
+        replace = require('gulp-replace'),
+        clean = require('gulp-clean'),
+        sass = require('gulp-sass'),
+        fs = require('fs'),
+
+        css = () => {
+            return src('./src/scss/*.scss')
+                .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+                .pipe(replace('@charset "UTF-8";\n', ''))
+                .pipe(autoprefixer({
+                    overrideBrowserslist: [
+                        'defaults', // > 0.5%, last 2 versions, Firefox ESR, not dead
+                        'IE 11'
+                    ],
+                    cascade: false
+                }))
+                .pipe(dest('./dist'));
+        },
+
+        js = () => {
+            return src('./src/js/**/*')
+                .pipe(dest('./dist/js'));
+        },
+
+        cleanDist = () => {
+            return src('./dist', { read: false })
+                .pipe(clean());
+        },
+
+        dist = () => {
+            return src([
+                './src/**/*.php',
+                './src/**/*.md',
+                './src/**/*.svg',
+                './src/**/*.png'
+            ])
+                .pipe(dest('./dist'))
+                .pipe(connect.reload());
+        },
+
+        reload = (done) => {
+            connect.reload();
+            done();
+        },
+
+        connectServer = (done) => {
+            connect.server({
+                root: 'dist',
+                https: {
+                    key: fs.readFileSync('_cert/localhost.key'),
+                    cert: fs.readFileSync('_cert/localhost.crt')
+                },
+                livereload: {
+                    enable: true,
+                    port: 8088
+                },
+                host: 'localhost',
+                port: 8080
+            });
+            done();
+        },
+
+        addWatcher = (done) => {
+            watch(['./src/**/*'], series(parallel(css, js), dist));
+            done();
+        };
+
+    sass.compiler = require('node-sass');
+
+    exports.deploy = series(cleanDist, parallel(css, js), dist);
+
+    exports.watch = series(
+        cleanDist,
+        parallel(css, js),
+        dist,
+        addWatcher,
+        connectServer
+    );
+
+
+})();
+
