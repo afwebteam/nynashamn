01fd942bbcdd2dad3c712063e332d1f1c08a5125
isse <anders.israelsson@afconsult.com>
2020-05-20 15:47:19 +0200

    More styling

--- src/js/afry.js
+++ src/js/afry.js
@@ -5,7 +5,8 @@ svDocReady(function () {
     'use strict';
 
     var jq = jQuery,
-        relatedFileLinks;
+        relatedFileLinks,
+        accordionContainer;
 
     jq('.sv-text-portlet.af-searchIcon').on('click', function (e) {
         jq('.af-hiddenSearchForm').toggle();
@@ -41,7 +42,57 @@ svDocReady(function () {
         });
     }
 
+    function addAccordionFunctions(accordionContainers) {
+
+        jq.each(accordionContainers, function (i, e) {
+
+            var container = jq(e),
+                items = container.find('.sv-font-stor-svart, h2, h3');
+
+            jq.each(items, function (index, elem) {
+                var item = jq(elem),
+                    span = jq('<span/>', {
+                        class: 'af-accordionText',
+                        text: 'Visa'
+                    });
+
+                item.attr('aria-expanded', false);
+                item.addClass('af-closed');
+                item.append(span);
+
+                item.on('click', function (e) {
+                    var target = jq(e.target),
+                        content = target.next('.normal'),
+                        text = target.find('.af-accordionText'),
+                        isExpanded = (item.attr('aria-expanded') === 'true');
+
+                    if (isExpanded) {
+                        item.attr('aria-expanded', false);
+                        content.hide();
+                        item.removeClass('af-open');
+                        text.text('Visa');
+                    } else {
+                        item.attr('aria-expanded', true);
+                        item.addClass('af-open');
+                        content.show();
+                        text.text('Dölj');
+                    }
+
+                });
+            });
+        });
+    }
+
     relatedFileLinks = jq('.af-related-information a[href*="/download/"]');
-    addFileInfoToFiles(relatedFileLinks);
+    accordionContainer = jq('.sv-decoration-generell-dragspel');
+
+    try {
+        addFileInfoToFiles(relatedFileLinks);
+    } catch (e) { }
+
+    try {
+        addAccordionFunctions(accordionContainer);
+    } catch (e) { }
+
 });
 
