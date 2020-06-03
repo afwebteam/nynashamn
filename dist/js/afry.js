f652b8f9a336b0fe667932078cf71f01e403bd95
isse <anders.israelsson@afconsult.com>
2020-06-03 13:30:21 +0200

    .

---
+++ dist/js/afry.js
@@ -0,0 +1,162 @@
+/* global svDocReady */
+
+svDocReady(function () {
+
+    'use strict';
+
+    var jq = jQuery,
+        relatedFileLinks,
+        accordionContainer;
+
+    jq('.sv-text-portlet.af-searchIcon').on('click', function (e) {
+        jq('.af-hiddenSearchForm').toggle();
+    });
+
+    // Link whole puff...
+    jq('.af-startpage-current .sv-channel-item').on('click', function (e) {
+
+        var target = jq(e.target),
+            link,
+            url;
+
+        if (!target.is('.sv-channel-item')) {
+            target = target.closest('.sv-channel-item');
+        }
+
+        link = target.find('a');
+        url = link.prop('href');
+        document.location.href = url;
+
+    });
+
+    // Link whole puff...
+    jq('.af-popularShortcuts--item').on('click', function (e) {
+
+        var target = jq(e.target),
+            link,
+            url;
+
+        if (!target.is('.af-popularShortcuts--item')) {
+            target = target.closest('.af-popularShortcuts--item');
+        }
+
+        link = target.find('a');
+        url = link.prop('href');
+        document.location.href = url;
+    });
+
+    function addFileInfoToFiles(aCollection) {
+
+        jq.each(aCollection, function (i, e) {
+
+            var target = jq(e),
+                title = target.attr('title'),
+                currentText = target.text(),
+                newText;
+
+            newText = currentText + ' (' + title + ')';
+            target.text(newText);
+        });
+    }
+
+    function addAccordionFunctions(accordionContainers) {
+
+        jq.each(accordionContainers, function (i, e) {
+
+            var container = jq(e),
+                items = container.find('.sv-font-stor-svart, h2, h3');
+
+            jq.each(items, function (index, elem) {
+                var item = jq(elem),
+                    span = jq('<span/>', {
+                        class: 'af-accordionText',
+                        text: 'Visa'
+                    });
+
+                item.attr('aria-expanded', false);
+                item.addClass('af-closed');
+                item.append(span);
+
+                item.on('click', function (e) {
+                    var target = jq(e.target),
+                        content = target.next('.normal'),
+                        text = target.find('.af-accordionText'),
+                        isExpanded = (item.attr('aria-expanded') === 'true');
+
+                    if (isExpanded) {
+                        item.attr('aria-expanded', false);
+                        content.hide();
+                        item.removeClass('af-open');
+                        text.text('Visa');
+                    } else {
+                        item.attr('aria-expanded', true);
+                        item.addClass('af-open');
+                        content.show();
+                        text.text('Dölj');
+                    }
+
+                });
+            });
+        });
+    }
+
+    relatedFileLinks = jq('.af-related-information a[href*="/download/"]');
+    accordionContainer = jq('.sv-decoration-generell-dragspel');
+
+    try {
+        addFileInfoToFiles(relatedFileLinks);
+    } catch (e) { }
+
+    try {
+        addAccordionFunctions(accordionContainer);
+    } catch (e) { }
+
+    jq('.sv-search-facets-header').on('click', function (e) {
+        var target = jq(e.target),
+            facets = jq('.sv-search-facet-backgroundColor'),
+            text = target.find('.af-accordionText'),
+            isExpanded;
+
+        if (!target.is('.sv-search-facets-header')) {
+            target = target.closest('.sv-search-facets-header');
+        }
+
+        isExpanded = (target.attr('aria-expanded') === 'true');
+
+        if (isExpanded) {
+            target.attr('aria-expanded', false);
+            facets.hide();
+            target.removeClass('af-open');
+            text.text('Visa');
+        } else {
+            target.attr('aria-expanded', true);
+            target.addClass('af-open');
+            facets.show();
+            text.text('Dölj');
+        }
+    });
+
+    jq('.af-MobileMenu').on('click', function (e) {
+
+        var theMenu = jq('.af-mobileMainMenu');
+
+        theMenu.toggle();
+    });
+
+    // Mobile menu
+    jq('.af-menuIcon').on('click', function (e) {
+
+        var target = jq(e.target),
+            container = target.closest('li'),
+            sublevel = container.find('ul').first(),
+            thisIcon = container.find('.af-menuIcon');
+
+        container.toggleClass('open');
+        sublevel.toggle();
+
+        console.log(container);
+
+    });
+
+});
+
